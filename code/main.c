76395cf3dd09deda4cb5803ad586f417ffe6adbd
Tobias Weis <tobi@taurus.lan>
2012-02-25 20:57:08 +0100

    PI-Controller and differential stuff to keep both wheels same speed

--- code/main.c
+++ code/main.c
@@ -12,12 +12,16 @@ uint16_t seconds = 0;
 uint16_t millis = 0;
 
 // ticks per 100ms
-uint8_t tpmsl = 0;
-uint8_t tpmsr = 0;
+int8_t tpmsl = 0;
+int8_t tpmsr = 0;
 
 // total ticks
 uint16_t ttl = 0;
 uint16_t ttr = 0;
+
+// sollticks
+int8_t sollticksl = 0;
+int8_t sollticksr = 0;
 
 // global speed-variables
 int8_t speedl = 0;
@@ -26,14 +30,14 @@ int8_t nspeedl = 0;
 int8_t nspeedr = 0;
 
 // sum of errors for i-controller
-uint16_t esuml = 0;
-uint16_t esumr = 0;
+int16_t esuml = 0;
+int16_t esumr = 0;
 
 /**************** Forward Decs *****************/
 void timer1_init(void);
 void set_clockdiv(uint8_t div);
 void set_speed(int8_t speed_left, int8_t speed_right);
-uint16_t pi(float kp, float ki, int error, float ta, uint16_t esum);
+int16_t pi(float kp, float ki, int error, float ta, int16_t esum);
 
 /**************** DEFINES **********************/
 #define RU 37.699 // Radumfang
@@ -65,64 +69,55 @@ ISR( INT1_vect){
 // Time
 ISR(TIMER3_COMPA_vect){
     millis++;
-    // count ticks for 10 ms for controller
-    if(millis % TIMESPAN == 0){
-        cli();
-            tpmsl = lcount;
-            lcount = 0;
-            tpmsr = rcount;
-            rcount = 0;
-        sei();
-            // increment total ticks (used later for distance measurement)
-            ttl += tpmsl;
-            ttr += tpmsr;
+	    // count ticks for 10 ms for controller
+	    if(millis % 10 == 0 ){
+		cli();
+		    tpmsl = lcount;
+		    lcount = 0;
+		    tpmsr = rcount;
+		    rcount = 0;
+		sei();
+		    // increment total ticks (used later for distance measurement)
+		    ttl += tpmsl;
+		    ttr += tpmsr;
 
-            // desired ticks per 10ms
-            int8_t sollticksl = 10;
-            int8_t sollticksr = 10;
+		    int32_t diff = ttl - ttr;
 
-            esuml += (sollticksl - tpmsl);
-            esumr += (sollticksr - tpmsr);
+		    esuml += sollticksl - tpmsl;
+		    esumr += sollticksr - tpmsr;
 
-            OCR1A = pi(0.5, 0.5, (sollticksl - tpmsl), (1/(timespan*10)), esuml);
-            OCR1B = pi(0.5, 0.5, (sollticksr - tpmsr), (1/(timespan*10)), esumr);
-    }
+		    int16_t new_speedl = OCR1A + pi(5.0f, 0.1f, (sollticksl - tpmsl), 1, -diff);
+		    int16_t new_speedr = OCR1B + pi(5.0f, 0.1f, (sollticksr - tpmsr), 1, diff);
+			if(new_speedl < 0) new_speedl = 0;
+			if(new_speedr < 0) new_speedr = 0;
+			if(new_speedl > 65535) new_speedl = 65535;
+			if(new_speedr > 65535) new_speedr = 65535;
+			OCR1A = new_speedl;
+			OCR1B = new_speedr;
+	    }
 
-    // show time
-    if (millis >= 500){
-        millis = 0;
-        seconds++;
-        char tmpstring[16];
-        LCDGotoXY(0, 1);
-        LCDstring("                ", 16);
-        LCDGotoXY(0, 1);
-        snprintf(tmpstring, 16, "S:%u L%u R%u", seconds, tpmsl, tpmsr);
-        LCDstring(tmpstring, strlen(tmpstring));
-
-        LCDGotoXY(0, 0);
-        LCDstring("                ", 16);
-        LCDGotoXY(0, 0);
-//        snprintf(tmpstring, 16, "LT%u RT%u", ttl, ttr);
-        snprintf(tmpstring, 16, "1A%u 1B%u", OCR1A, OCR1B);
-        LCDstring(tmpstring, strlen(tmpstring));
-    }
 }
 
 /*************** HELPER FUNCTIONS ********************/
-uint16_t pi(float kp, float ki, int error, float ta, uint16_t esum){
-    uint8_t corrected = kp * error + ki * ta * esum;
-    uint16_t new_value = corrected * 1024 * (10.0f/timespan);
+int16_t pi(float kp, float ki, signed int error, float ta, int16_t esum){
+    float corrected = kp * error + ki * esum;
+    int32_t new_value = corrected * 1024;
 
-    if(new_value > 65535) new_value = 65535;
-    if(new_value < 0) new_value = 0;
+    if(new_value > 31199) new_value = 31199;
+    if(new_value < -31199) new_value = -31199;
 
     return new_value;
 }
 
 void timer1_init(void){
     //Modus und Vorteiler wählen (Phase Correct PWM und Vorteiler 1)
-    TCCR1A |= (1<<COM1A1)|(1<<COM1B1)|(1<<COM1C1)|(1<<WGM11);
-    TCCR1B |= (1<<WGM13)|(1<<CS10);
+    //TCCR1A |= (1<<COM1A1)|(1<<COM1B1)|(1<<COM1C1)|(1<<WGM11);
+    TCCR1A |= (1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);
+    TCCR1A &= ~(1<<COM1A0);
+    TCCR1A &= ~(1<<COM1B0);
+
+    TCCR1B |= (1<<WGM12)|(1<<WGM13)|(1<<CS10);
+
     //Auflösung 16-Bit
     ICR1 |= 0xFFFF;
 }
@@ -138,26 +133,6 @@ void set_clockdiv(uint8_t div){
 }
 
 
-// TO BE REPLACED !
-void set_speed(int8_t speed_left, int8_t speed_right){
-    if(speed_left > 16) speed_left = 16;
-    if(speed_left < 0) speed_left = 0;
-    if(speed_right > 16) speed_right = 16;
-    if(speed_right < 0) speed_right = 0;
-
-    if(speed_left * SSTEP > 65535){
-        OCR1A = 65535;
-    }else{
-        OCR1A = speed_left * SSTEP; // speed * bits per volt
-    }
-
-    if(speed_right * SSTEP > 65535){
-        OCR1B = 65535;
-    }else{
-        OCR1B = speed_right * SSTEP; // speed * bits per volt
-    }
-}
-
 void set_direction(int8_t dir){
     // zurück
     if(dir == -1){
@@ -169,11 +144,11 @@ void set_direction(int8_t dir){
     }
     /* bremsen */
     if(dir == 0){
-        PORTD |= (1 << 5);
-        PORTD |= (1 << 4);
+        PORTD &= ~(1 << 5);
+        PORTD &= ~(1 << 4);
 
-        PORTD |= (1 << 7);
-        PORTD |= (1 << 6);
+        PORTD &= ~(1 << 7);
+        PORTD &= ~(1 << 6);
     }
     /* vorwärts */
     if(dir == 1){
@@ -189,12 +164,10 @@ void trigger(){
         PORTB &= ~(1 << 1);
         PORTB &= ~(1 << 2);
         PORTB &= ~(1 << 3);
-        PORTB |= (1 << 0); //set pin 0 on port B high
         _delay_ms(1000);
         PORTB |= (1 << 1);
         PORTB |= (1 << 2);
         PORTB |= (1 << 3);
-        PORTB &= ~(1 << 0); //set pin 0 on port B low
 }
 
 
@@ -210,14 +183,14 @@ int main (void){
     
     // Motor-Leitungen auf Ausgang stellen
     DDRD |= (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
-    //DDRD &= ~(1 << 0); // eingang
-    //DDRD &= ~(1 << 1); // eingang
+    DDRD &= ~(1 << 0); // eingang
+    DDRD &= ~(1 << 1); // eingang
     PORTD |= (1 << 0); // pullup
     PORTD |= (1 << 1); // pullup
 
     // interrupts einschalten
-    EIMSK |= (1 << INT0) | (1 << INT1); // INT0 enable
     EICRA |= (1 << ISC10) | (1 << ISC00); 
+    EIMSK |= (1 << INT0) | (1 << INT1); // INT0 enable
 
     // Timer 3 für die Uhrzeit
     TCCR3A = 0;
@@ -226,6 +199,9 @@ int main (void){
     TCCR3B |= (1 << WGM12);
     TCCR3B |= (1 << CS10);
     TIMSK3 |= (1 << OCIE3A);
+
+	OCR1A = 0;
+	OCR1B = 0;
 
     // Interrupt starten
     sei();
@@ -237,41 +213,58 @@ int main (void){
 
     while (1){
         set_direction(1);
-        speedl = 10;
-        speedr = 10;
-
-        OCR1A = 50000;
-        OCR1B = 50000;
 
         char string[16];
 
         while(1){
-            // controller for the wheels
-            cli();
-                uint16_t mytpmsl = tpmsl;
-                uint16_t mytpmsr = tpmsr;
-            sei();
+		set_direction(1);
+		sollticksl = 10;
+		sollticksr = 10;
 
-            // get total ticks
-            cli();
-                uint16_t my_ttl = ttl;
-            sei();
-            if(my_ttl >= 10*TPU){ // halbe umdrehung
-                set_direction(0);
+	    cli();
+		uint16_t my_millis = millis;
+	    sei();
 
-                // AUSGABE AUF DISPLAY
-                /*
-                LCDGotoXY(0, 0);
-                snprintf(string, 16, "L:%u R:%u S:%u", ttl, ttr ,shotcount);
-                LCDstring("                ", 16);
-                LCDGotoXY(0, 0);
-                LCDstring(string, strlen(string));
-                */
+
+	    // show time
+	    if (my_millis >= 1000){
+		cli();
+			millis = 0;
+		sei();
+		seconds++;
+		char tmpstring[16];
+		LCDGotoXY(0, 1);
+		LCDstring("                ", 16);
+		LCDGotoXY(0, 1);
+		snprintf(tmpstring, 16, "S:%u L%u R%u", seconds, tpmsl, tpmsr);
+		LCDstring(tmpstring, strlen(tmpstring));
+
+		LCDGotoXY(0, 0);
+		LCDstring("                ", 16);
+		LCDGotoXY(0, 0);
+		snprintf(tmpstring, 16, "LT%u RT%u", ttl, ttr);
+		LCDstring(tmpstring, strlen(tmpstring));
+	    }
+
+
+            if(ttl >= 3600){
+		set_direction(0);
+		sollticksl = 0;
+		sollticksr = 0;
+		esumr = 0;
+		esuml = 0;
+
+		char tmpstring[16];
+		LCDGotoXY(0, 0);
+		LCDstring("                ", 16);
+		LCDGotoXY(0, 0);
+		snprintf(tmpstring, 16, "LT%u RT%u", ttl, ttr);
+		LCDstring(tmpstring, strlen(tmpstring));
+
 
                 _delay_ms(2000);
                 trigger();
                 shotcount++;
-                set_direction(1);
                 cli();
                     ttl = 0;
                     ttr = 0;
